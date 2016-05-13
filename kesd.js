faf6179cc1e620de3bbc214efa6260c32f8bb942
Nick Hsieh <mosabeker@gmail.com>
2016-05-13 21:56:44 +0800

    update

--- kesd.js
+++ kesd.js
@@ -1,9 +1,6 @@
-
-
-
-
-
 var randomValue=Math.random();
+var A = 0;
+var B = 0;
 
 randomValue=Math.floor(randomValue*10000);
 randomValue=1234
@@ -11,22 +8,54 @@ randomValue=1234
 console.log(String(Math.floor(randomValue)));
 var randomString = String(Math.floor(randomValue));
 
-console.log(randomString.length);
+//console.log(randomString.length);
 
+/*
+var checkValue = function(i)
+{
 
+	if(randomString[i]===guessValueString[i])
+					{
+						A++;
+					}
+					else
+					{	
 
+						for (var temp=i; temp < randomString.length; temp++)
+						{
+							if(randomString[i]===guessValueString[temp])
+								B++;
+						} 
+
+					}
+
+} 
+*/
 
 var testValue = function(guessValue)
 {
 	var guessValueString = guessValue.toString();
+	
 
 	for (var i = 0; i < randomString.length; i++) 
 	{ 
         
-		switch(randomString[i]) 
+
+		switch(i) 
 		{
-			case 0: console.log(i);
+			case 0: 
+
+					checkValue(i);
+				
 					break;
+			case 1:
+						break;	
+			case 2:
+				break;
+
+			case 3:	
+				break;		
+
 
 	//		case ath.floor(randomValue-(randomValue/1000)): abc = 3;
 	//		case Math.floor((randomValue/100)): abc--;
@@ -39,5 +68,5 @@ var testValue = function(guessValue)
 	return 1;
 }
 
-console.log(testValue(randomString));
+testValue(1234);
 
